c25d81294e94a5bcf7c00d1cd9d142609bdb737f
parkjihwan <mankcom7708@gmail.com>
2024-01-16 10:55:23 +0900

    Test #1 : dockerfile test

--- src/main/java/com/example/airdnstest/AirDnsTestApplication.java
+++ src/main/java/com/example/airdnstest/AirDnsTestApplication.java
@@ -5,7 +5,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class AirDnsTestApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(AirDnsTestApplication.class, args);
 	}
